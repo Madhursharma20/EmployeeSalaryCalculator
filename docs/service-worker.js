a2bcecb438e8aebfd3b6a4f3fd77d651734fc04b
Madhur Sharma <91717930+Madhursharma20@users.noreply.github.com>
2025-01-30 20:11:22 +0530

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,10 +14,8 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match("index.html"))
+    );
 });
